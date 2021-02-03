376e7688af3362b4d002573f437b8405b78261e6
Sandra-Louchart <sandra.louchart@gmail.com>
2021-02-03 19:52:14 +0100

    security route

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AdministrativeRepository;
+use App\Repository\CompetenceRepository;
+use App\Repository\FormationRepository;
+use App\Repository\ProjectRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class AdminController
+ * @package App\Controller
+ * @Route("/admin", name="admin_")
+ * @IsGranted("ROLE_ADMIN")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     * @param AdministrativeRepository $administrativeRepository
+     * @param CompetenceRepository $competenceRepository
+     * @param FormationRepository $formationRepository
+     * @param ProjectRepository $projectRepository
+     * @return Response
+     */
+    public function index(AdministrativeRepository $administrativeRepository, CompetenceRepository $competenceRepository, FormationRepository $formationRepository,
+ProjectRepository $projectRepository)
+    {
+        return $this->render('admin/index.html.twig', [
+            'formations'=>$formationRepository->findAll(),
+            'competences'=>$compentenceRepository->findAll(),
+            'administratives'=>$administrativeRepository->findAll(),
+            'projects'=>$projectRepository->findAll(),
+        ]);
+    }
+}
